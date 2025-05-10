fb3908877f3d39c55bf25e5fed935171004a2f0c
Santiago Bustos Pianda <119011448+piandas@users.noreply.github.com>
2025-05-10 09:49:15 -0500

    Refactor: reorganizar la estructura del código y agregar funciones de análisis y lectura de líneas

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,1 +1,12 @@
+import os
 
+from homework.src._internals.parse_args import parse_args
+from homework.src._internals.read_all_lines import read_all_lines
+
+
+def main():
+    input_folder, output_folder = parse_args()
+    lines = read_all_lines(input_folder)
+
+    print(f"Input folder: {input_folder}")
+    print(f"Output folder: {output_folder}")
